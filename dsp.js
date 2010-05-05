ae325237ba6f263c7f24bf1dd0740b11d6ba7b26
Corban Brook <corbanbrook@gmail.com>
2010-05-05 16:05:13 -0400

    Added many more window functions.

--- dsp.js
+++ dsp.js
@@ -21,7 +21,16 @@ DSP.BANDPASS = 2;
 DSP.NOTCH    = 3;
 
 // Window functions
-DSP.HANN     = 7;
+DSP.HANN         =  7;
+DSP.HAMMING      =  8;
+DSP.RECTANGULAR  =  9;
+DSP.TRIANGULAR   = 10;
+DSP.BLACKMAN     = 11;
+DSP.BARTLETT     = 12;
+DSP.BARTLETTHANN = 13;
+DSP.GAUSS        = 14;
+DSP.LANCZOS      = 15;
+DSP.COSINE       = 16;
 
 DSP.TWO_PI = 2*Math.PI;
 
@@ -526,10 +535,50 @@ IIRFilter2.prototype.set = function(cutoff, resonance) {
   this.calcCoeff(cutoff, resonance); 
 };
 
-WindowFunction = function(type, params) {
+WindowFunction = function(type, alpha) {
+  this.alpha = alpha;
+  
   switch(type) {
     case DSP.HANN:
       this.func = WindowFunction.Hann;
+      break;
+    
+    case DSP.HAMMING:
+      this.func = WindowFunction.Hamming;
+      break;
+      
+    case DSP.COSINE:
+      this.func = WindowFunction.Cosine;
+      break;
+      
+    case DSP.RECTANGULAR:
+      this.func = WindowFunction.Rectangular;
+      break;
+      
+    case DSP.TRIANGULAR:
+      this.func = WindowFunction.Triangular;
+      break;
+    
+    case DSP.LANCZOS:
+      this.func = WindowFunction.Lanczoz;
+      break;
+      
+    case DSP.BARTLETT:
+      this.func = WindowFunction.Bartlett;
+      break;
+      
+    case DSP.BARTLETTHANN:
+      this.func = WindowFunction.BartlettHann;
+      break;
+      
+    case DSP.BLACKMAN:
+      this.func = WindowFunction.Blackman;
+      this.alpha = this.alpha || 0.16;
+      break;
+          
+    case DSP.GAUSS:
+      this.func = WindowFunction.Gauss;
+      this.alpha = this.alpha || 0.25;
       break;
   }
 };
@@ -537,11 +586,51 @@ WindowFunction = function(type, params) {
 WindowFunction.prototype.process = function(buffer) {
   var length = buffer.length;
   for ( var i = 0; i < length; i++ ) {
-    buffer[i] *= this.func(i, length);
+    buffer[i] *= this.func(length, i, this.alpha);
   }
 };
 
-WindowFunction.Hann = function(index, length) {
+WindowFunction.Rectangular = function(length, index) {
+  return 1;
+};
+
+WindowFunction.Triangular = function(length, index) {
+  return 2 / length * (length / 2 - Math.abs(index - (length - 1) / 2));
+};
+
+WindowFunction.Hann = function(length, index) {
   return 0.5 * (1 - Math.cos(DSP.TWO_PI * index / (length - 1)));
 };
 
+WindowFunction.Hamming = function(length, index) {
+  return 0.54 - 0.46 * Math.cos(DSP.TWO_PI * index / (length - 1));
+};
+
+WindowFunction.Lanczos = function(length, index) {
+  var x = 2 * index / (length - 1) - 1;
+  return Math.sin(Math.PI * x) / (Math.PI * x);
+};
+
+WindowFunction.Cosine = function(length, index) {
+  return Math.cos(Math.PI * index / (length - 1) - Math.PI / 2);
+};
+
+WindowFunction.Bartlett = function(length, index) {
+  return 2 / (length - 1) * ((length - 1) / 2 - Math.abs(index - (length - 1) / 2));
+};
+
+WindowFunction.BartlettHann = function(length, index) {
+  return 0.62 - 0.48 * Math.abs(index / (length - 1) - 0.5) - 0.38 * Math.cos(DSP.TWO_PI * index / (length - 1));
+};
+
+WindowFunction.Blackman = function(length, index, alpha) {
+  var a0 = (1 - alpha) / 2;
+  var a1 = 0.5;
+  var a2 = alpha / 2;
+
+  return a0 - a1 * Math.cos(DSP.TWO_PI * index / (length - 1)) + a2 * Math.cos(4 * Math.PI * index / (length - 1));
+};
+
+WindowFunction.Gauss = function(length, index, alpha) {
+  return Math.pow(Math.E, -0.5 * Math.pow((index - (length - 1) / 2) / (alpha * (length - 1) / 2), 2));
+};
