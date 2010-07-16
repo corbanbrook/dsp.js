6c11c043b98e964399a5ac7c1a3fc03deb23053b
Almer Thie <almeros@gmail.com>
2010-07-16 18:29:47 +0200

    Adding SingleDelay and Reverb classes

--- dsp.js
+++ dsp.js
@@ -1510,8 +1510,9 @@ GraphicalEq = function(sampleRate) {
 
 
 /**
- * Delay effect by Almer Thie (http://code.almeros.com).
+ * MultiDelay effect by Almer Thie (http://code.almeros.com).
  * Copyright 2010 Almer Thie. All rights reserved.
+ * Example: http://code.almeros.com/code-examples/delay-firefox-audio-api/
  * 
  * This is a delay that feeds it's own delayed signal back into its circular 
  * buffer. Also known as a CombFilter.
@@ -1559,7 +1560,7 @@ MultiDelay.prototype.setMasterVolume = function (masterVolume){
 }
 
 /**
- * Change the master volume.
+ * Change the delay feedback volume.
  *
  * @param {Number} delayVolume Float value: 0.0 (silence), 1.0 (normal), >1.0 (amplify)
  */
@@ -1605,3 +1606,276 @@ MultiDelay.prototype.process = function (samples){
 	
 	return outputSamples;
 }
+
+
+
+
+/**
+ * SingleDelay effect by Almer Thie (http://code.almeros.com).
+ * Copyright 2010 Almer Thie. All rights reserved.
+ * Example: See usage in Reverb class
+ * 
+ * This is a delay that does NOT feeds it's own delayed signal back into its  
+ * circular buffer, neither does it return the original signal. Also known as 
+ * an AllPassFilter(?).
+ *
+ * Compatible with interleaved stereo (or more channel) buffers and 
+ * non-interleaved mono buffers.
+ * 
+ * @param {Number} maxDelayInSamplesSize Maximum possible delay in samples (size of circular buffer)
+ * @param {Number} delayInSamples Initial delay in samples
+ * @param {Number} delayVolume Initial feedback delay volume. Float value: 0.0 (silence), 1.0 (normal), >1.0 (amplify)
+ * 
+ * @constructor
+ */
+SingleDelay = function(maxDelayInSamplesSize, delayInSamples, delayVolume){
+	this.delayBufferSamples 	= new Float32Array(maxDelayInSamplesSize); // The maximum size of delay
+	this.delayInputPointer 		= delayInSamples; 
+	this.delayOutputPointer 	= 0;
+	
+	this.delayInSamples 	= delayInSamples;
+	this.delayVolume 		= delayVolume;
+}
+
+/**
+ * Change the delay time in samples.
+ *
+ * @param {Number} delayInSamples Delay in samples
+ */
+SingleDelay.prototype.setDelayInSamples = function (delayInSamples){
+	this.delayInSamples = delayInSamples;
+	
+	this.delayInputPointer = this.delayOutputPointer + delayInSamples;
+	if(this.delayInputPointer >= this.delayBufferSamples.length-1)
+		this.delayInputPointer = this.delayInputPointer - this.delayBufferSamples.length;	
+}
+
+/**
+ * Change the return signal volume.
+ *
+ * @param {Number} delayVolume Float value: 0.0 (silence), 1.0 (normal), >1.0 (amplify)
+ */
+SingleDelay.prototype.setDelayVolume = function (delayVolume){
+	this.delayVolume = delayVolume;
+}
+
+/**
+ * Process a given interleaved or mono non-interleaved float value Array and 
+ * returns the delayed audio.
+ *
+ * @param {Array} samples Array containing Float values or a Float32Array
+ * 
+ * @returns A new Float32Array interleaved or mono non-interleaved as was fed to this function.
+ */
+SingleDelay.prototype.process = function (samples){
+	// NB. Make a copy to put in the output samples to return. 
+	var outputSamples = new Float32Array(samples.length);
+
+	for(var i=0; i<samples.length; i++){
+
+		// Add audio data with the delay in the delay buffer
+		this.delayBufferSamples[this.delayInputPointer] = samples[i];
+		
+		// delayBufferSamples could contain initial NULL's, return silence in that case
+		var delaySample = this.delayBufferSamples[this.delayOutputPointer];
+
+		// Return the audio with delay mix
+		outputSamples[i] = delaySample * this.delayVolume;
+
+		
+		// Manage circulair delay buffer pointers
+		this.delayInputPointer++;
+		if(this.delayInputPointer >= this.delayBufferSamples.length-1)
+			this.delayInputPointer = 0;
+			
+		this.delayOutputPointer++;
+		if(this.delayOutputPointer >= this.delayBufferSamples.length-1)
+			this.delayOutputPointer = 0;	
+			
+	}
+	
+	return outputSamples;
+}
+
+
+
+
+
+/**
+ * Reverb effect by Almer Thie (http://code.almeros.com).
+ * Copyright 2010 Almer Thie. All rights reserved.
+ * Example: http://code.almeros.com/code-examples/reverb-firefox-audio-api/
+ * 
+ * This reverb consists of 6 SingleDelays, 6 MultiDelays and an IIRFilter2 
+ * for each of the two stereo channels.
+ *
+ * Compatible with interleaved stereo buffers only!
+ * 
+ * @param {Number} maxDelayInSamplesSize Maximum possible delay in samples (size of circular buffers)
+ * @param {Number} delayInSamples Initial delay in samples for internal (Single/Multi)delays
+ * @param {Number} masterVolume Initial master volume. Float value: 0.0 (silence), 1.0 (normal), >1.0 (amplify)
+ * @param {Number} mixVolume Initial reverb signal mix volume. Float value: 0.0 (silence), 1.0 (normal), >1.0 (amplify)
+ * @param {Number} delayVolume Initial feedback delay volume for internal (Single/Multi)delays. Float value: 0.0 (silence), 1.0 (normal), >1.0 (amplify)
+ * @param {Number} dampFrequency Initial low pass filter frequency. 0 to 44100 (depending on your maximum sampling frequency)
+ * 
+ * @constructor
+ */
+Reverb = function(maxDelayInSamplesSize, delayInSamples, masterVolume, mixVolume, delayVolume, dampFrequency){
+	this.delayInSamples 	= delayInSamples;
+	this.masterVolume 		= masterVolume;
+	this.mixVolume 			= mixVolume;
+	this.delayVolume 		= delayVolume;
+	this.dampFrequency 		= dampFrequency;
+	
+	this.NR_OF_MULTIDELAYS = 6;
+	this.NR_OF_SINGLEDELAYS = 6;
+	
+	this.LOWPASSL = new IIRFilter2(DSP.LOWPASS, dampFrequency, 0, 44100);
+	this.LOWPASSR = new IIRFilter2(DSP.LOWPASS, dampFrequency, 0, 44100);
+	
+	this.singleDelays = [];
+	for(var i = 0; i<this.NR_OF_SINGLEDELAYS; i++){
+		var delayMultiply = 1.0 + (i/7.0); // 1.0, 1.1, 1.2... 
+		this.singleDelays[i] = new SingleDelay(maxDelayInSamplesSize, Math.round(this.delayInSamples * delayMultiply), this.delayVolume);
+	}
+	
+	this.multiDelays = [];
+	for(var i = 0; i<this.NR_OF_MULTIDELAYS; i++){
+		var delayMultiply = 1.0 + (i/10.0); // 1.0, 1.1, 1.2...  
+		this.multiDelays[i] = new MultiDelay(maxDelayInSamplesSize, Math.round(this.delayInSamples * delayMultiply), this.masterVolume, this.delayVolume);
+	}
+}
+
+/**
+ * Change the delay time in samples as a base for all delays.
+ *
+ * @param {Number} delayInSamples Delay in samples
+ */
+Reverb.prototype.setDelayInSamples = function (delayInSamples){
+	this.delayInSamples = delayInSamples;
+	
+	for(var i = 0; i<this.NR_OF_SINGLEDELAYS; i++){
+		var delayMultiply = 1.0 + (i/7.0); // 1.0, 1.1, 1.2...
+		this.singleDelays[i].setDelayInSamples( Math.round(this.delayInSamples * delayMultiply) );
+	}
+		
+	for(var i = 0; i<this.NR_OF_MULTIDELAYS; i++){
+		var delayMultiply = 1.0 + (i/10.0); // 1.0, 1.1, 1.2...
+		this.multiDelays[i].setDelayInSamples( Math.round(this.delayInSamples * delayMultiply) );
+	}
+}
+
+/**
+ * Change the master volume.
+ *
+ * @param {Number} masterVolume Float value: 0.0 (silence), 1.0 (normal), >1.0 (amplify)
+ */
+Reverb.prototype.setMasterVolume = function (masterVolume){
+	this.masterVolume = masterVolume;
+}
+
+/**
+ * Change the reverb signal mix level.
+ *
+ * @param {Number} mixVolume Float value: 0.0 (silence), 1.0 (normal), >1.0 (amplify)
+ */
+Reverb.prototype.setMixVolume = function (mixVolume){
+	this.mixVolume = mixVolume;
+}
+
+/**
+ * Change all delays feedback volume.
+ *
+ * @param {Number} delayVolume Float value: 0.0 (silence), 1.0 (normal), >1.0 (amplify)
+ */
+Reverb.prototype.setDelayVolume = function (delayVolume){
+	this.delayVolume = delayVolume;
+	
+	for(var i = 0; i<this.NR_OF_SINGLEDELAYS; i++){
+		this.singleDelays[i].setDelayVolume(this.delayVolume);
+	}	
+	
+	for(var i = 0; i<this.NR_OF_MULTIDELAYS; i++){
+		this.multiDelays[i].setDelayVolume(this.delayVolume);
+	}	
+}
+
+/**
+ * Change the Low Pass filter frequency.
+ *
+ * @param {Number} dampFrequency low pass filter frequency. 0 to 44100 (depending on your maximum sampling frequency)
+ */
+Reverb.prototype.setDampFrequency = function (dampFrequency){
+	this.dampFrequency = dampFrequency;
+	
+	this.LOWPASSL.set(dampFrequency, 0);
+	this.LOWPASSR.set(dampFrequency, 0);	
+}
+
+/**
+ * Process a given interleaved float value Array and copies and adds the reverb signal.
+ *
+ * @param {Array} samples Array containing Float values or a Float32Array
+ * 
+ * @returns A new Float32Array interleaved buffer.
+ */
+Reverb.prototype.process = function (interleavedSamples){	
+	// NB. Make a copy to put in the output samples to return. 
+	var outputSamples = new Float32Array(interleavedSamples.length);
+	
+	// Perform low pass on the input samples to mimick damp
+	var leftRightMix = DSP.deinterleave(interleavedSamples);
+	this.LOWPASSL.process( leftRightMix[DSP.LEFT] );
+	this.LOWPASSR.process( leftRightMix[DSP.RIGHT] );	
+	var filteredSamples = DSP.interleave(leftRightMix[DSP.LEFT], leftRightMix[DSP.RIGHT]);
+
+	// Process MultiDelays in parallel
+	for(var i = 0; i<this.NR_OF_MULTIDELAYS; i++){
+		// Invert the signal of every even multiDelay
+		outputSamples = mixSampleBuffers(outputSamples, this.multiDelays[i].process(filteredSamples), 2%i==0, this.NR_OF_MULTIDELAYS);
+	}
+	
+	// Process SingleDelays in series
+	var singleDelaySamples = new Float32Array(outputSamples.length);
+	for(var i = 0; i<this.NR_OF_SINGLEDELAYS; i++){
+		// Invert the signal of every even singleDelay
+		singleDelaySamples = mixSampleBuffers(singleDelaySamples, this.singleDelays[i].process(outputSamples), 2%i==0, 1); 
+	}
+
+	// Apply the volume of the reverb signal
+	for(var i = 0; i<singleDelaySamples.length; i++){
+		singleDelaySamples[i] *= this.mixVolume;
+	}
+	
+	// Mix the original signal with the reverb signal
+	outputSamples = mixSampleBuffers(singleDelaySamples, interleavedSamples, 0, 1); 
+
+	// Apply the master volume to the complete signal
+	for(var i = 0; i<outputSamples.length; i++){
+		outputSamples[i] *= this.masterVolume;
+	}
+		
+	return outputSamples;
+}
+
+/**
+ * Helper method (for Reverb) to mix two (interleaved) samplebuffers. It's possible 
+ * to negate the second buffer while mixing and to perform a volume correction 
+ * on the final signal.
+ * 
+ * @param {Array} sampleBuffer1 Array containing Float values or a Float32Array
+ * @param {Array} sampleBuffer2 Array containing Float values or a Float32Array
+ * @param {Boolean} negate When true inverts/flips the audio signal
+ * @param {Number} volumeCorrection When you add multiple sample buffers, use this to tame your signal ;)
+ * 
+ * @returns A new Float32Array interleaved buffer.
+ */
+function mixSampleBuffers(sampleBuffer1, sampleBuffer2, negate, volumeCorrection){
+	var outputSamples = new Float32Array(sampleBuffer1);
+
+	for(var i = 0; i<sampleBuffer1.length; i++){
+		outputSamples[i] += (negate ? -sampleBuffer2[i] : sampleBuffer2[i]) / volumeCorrection;
+	}
+	
+	return outputSamples;
+}	
